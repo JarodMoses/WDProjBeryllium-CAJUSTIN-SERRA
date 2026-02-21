59bd284060c4eeba70b7df3576ec151b88afe291
BenedictSerra <b2029bjvserra@pshs.edu.ph>
2026-02-21 12:49:40 +0800

    Final Commit (3rd Q) (hopefully)

--- public/Accounts.js
+++ public/Accounts.js
@@ -5,14 +5,9 @@ const filterRole = document.getElementById("filterRole");
 const total = document.getElementById("total");
 
   function getSignups() {
-    return JSON.parse(localStorage.getItem("signups")) || [];
+    return JSON.parse(localStorage.getItem("signups")) || []; /* Retrieve item key "signups" from storage */
   }
 
-
-  function showSignups() {
-    viewSection.style.display = "block";
-    displayTable(getSignups());
-  }
 
   function displayTable(data) {
     tableBody.innerHTML = "";
@@ -30,29 +25,29 @@ const total = document.getElementById("total");
     total.textContent = "Total Sign-ups: " + data.length;
   }
 
-    window.onload = function() {
+    window.onload = function() {  /*Load the accounts table on window load */
     displayTable(getSignups());
     };
 
-    function applyFilters() {
+    function applyFilters() {    /*Filter function for filtlers */
     const signups = getSignups();
-
+                                          /*the three filters */
     const regionVal = filterRegion.value;
     const rankVal = filterRank.value;
     const roleVal = filterRole.value;
 
-    const filtered = signups.filter(s =>
+    const filtered = signups.filter(s =>  /*Checking for objects that match the filters */
         (regionVal === "All" || s.region === regionVal) &&
         (rankVal === "All" || s.rank === rankVal) &&
         (roleVal === "All" || s.role === roleVal)
     );
 
-    displayTable(filtered);
+    displayTable(filtered); /*Displays the objects that do match up with the filters */
 
 
 }
 
 
-filterRegion.addEventListener("change", applyFilters);
+filterRegion.addEventListener("change", applyFilters);  /*Do the function applyFilters when drowpdown is modified*/
 filterRank.addEventListener("change", applyFilters);
 filterRole.addEventListener("change", applyFilters);
--- public/Create.js
+++ public/Create.js
@@ -6,25 +6,25 @@
     const rank = document.getElementById("rank");
 
     function getSignups() {
-      return JSON.parse(localStorage.getItem("signups")) || [];
+      return JSON.parse(localStorage.getItem("signups")) || [];  // retrieve data
     }
 
     function saveSignups(data) {
-      localStorage.setItem("signups", JSON.stringify(data));
+      localStorage.setItem("signups", JSON.stringify(data));   // save data in local storage
     }
 
-    function selectPlayer(playerName, element) {
+    function selectPlayer(playerName, element) {     //Highlights chosen player in signups form
       document.querySelectorAll(".player img")
-        .forEach(img => img.classList.remove("selected"));
+        .forEach(img => img.classList.remove("selected"));   // So there arent two highlights
 
       element.classList.add("selected");
       player.value = playerName;
-      playerchosen.textContent = "Selected Player: " + playerName;
+      playerchosen.textContent = "Selected Player: " + playerName;  //displays text of chosen player
     }
 
     function resetPlayer() {
       document.querySelectorAll(".player img")
-        .forEach(img => img.classList.remove("selected"));
+        .forEach(img => img.classList.remove("selected"));   //remove selected border and txt
       player.value = "";
       playerchosen.textContent = "";
     }
@@ -35,11 +35,33 @@
     form.addEventListener("submit", function(e) {
       e.preventDefault();
 
+    const signups = getSignups();
 
-        if (player.value === "") {
-            alert("Please select a player.");
-            return; 
-        }
+    
+    const EmailToCheck = email.value.trim().toLowerCase();   //CONVERTS EMAIL VALUE TO LOWERCASE FOR CHECKING PURPOSES
+    const EmailExists = signups.some(user => (user.email || "").toLowerCase() === EmailToCheck); // compares values in signups to the email check
+
+    const UsernameToCheck = username.value.trim().toLowerCase(); // Converts username value to lowercase for checking purposes
+    const UsernameExists = signups.some(user => (user.username || "").toLowerCase() === UsernameToCheck) // compares values in signups to the username check
+
+    
+    if (EmailExists && UsernameExists) { // if both user and email are already taken
+      alert("That email and username are both taken. Please choose a different email username")
+      return;
+    }
+
+    else if (EmailExists) {  // if email is already taken
+      alert("That email is already taken. Please use another one.");
+      return; 
+    }
+
+    else if (UsernameExists) // if username is already taken
+    {
+      alert("That username is already taken. Please choose another one")
+      return;
+    }
+
+
 
     const confirmed = confirm("Are you sure you want to create this account?");
 
@@ -47,23 +69,29 @@
         return; // if they decline stop
     }
 
-      const signups = getSignups();
+
     
       const user = {
         username: username.value,
         role: role.value,
+        email: email.value,
         region: region.value,
         rank: rank.value,
-        prxplayer: player.value,
+        prxplayer: player.value,  /*SAVING USER DATA FOR TABLE DISPLAY IN ACCOUNTS.HTML*/
 
       };
 
       
 
-      signups.push(user);
-      saveSignups(signups);
+      signups.push(user);   
+      saveSignups(signups); /* Saving */
 
       alert("Signup saved!");
       form.reset();
       resetPlayer()
+
+      window.scrollTo({  /*Scroll back to top after account creation */
+            top: 0,
+            behavior: "smooth"
+        });
     });
