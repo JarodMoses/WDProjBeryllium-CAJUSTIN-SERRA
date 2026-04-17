628bbf46d3e0a5ec68ac9aa4883fcf2e2552dd1d
BenedictSerra <b2029bjvserra@pshs.edu.ph>
2026-04-17 12:11:57 +0800

    Final Commit --- added account excluvisity.

--- public/Global.js
+++ public/Global.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const account = document.getElementById("accountLink");  //basically changing all the "Create/LOGIN to "MyProfile" when logged in
+    const accountlist = document.getElementById("AccountsList")
+    const navlinks = document.getElementById("navlinks")
 
+
+    
     const loggedin = JSON.parse(localStorage.getItem("matcheduser"));
+
+    if (!loggedin)
+    {
+        account.style.margin = "auto auto auto -2.2vw";
+    }
     if (loggedin && account) { //if user logged in change Create/LOGIN to My Profile and its link to Profile.html
         account.textContent = "My Profile";
         account.href = "Profile.html";
+        accountlist.style.display = "block";
+        navlinks.style.gap = "50px";
     }
 })
 
